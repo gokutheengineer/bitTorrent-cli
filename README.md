6d36d93d5725d989671e089f17cec57e14fbcc5a
gokutheengineer <engineerzubizaretta@gmail.com>
2024-11-04 08:11:20 +0300

    feat: parse torrent file, with info command

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ A simple command line interface for downloading torrents using the BitTorrent pr
 
 
 Commands:
-1. "decode" - Decodes bencoded string. Example: ```shell ./bittorrent.sh decode 6:winter```
+1. "decode" - Decodes bencoded number/string/list/dictionary. Example: ```./bittorrent.sh decode 6:winter```
+2. "info" - Parses the torrent file and prints the info hash. Example: ```./bittorrent.sh info ./test.torrent```
--- cmd/bittorrent/main.go
+++ cmd/bittorrent/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"github.com/jackpal/bencode-go"
 	"os"
 	"strings"
 )
+
+type TorrentFile struct {
+	bencodeMap map[string]interface{}
+	infoMap    map[string]interface{}
+	encodedMap bytes.Buffer
+}
 
 func main() {
 	command := os.Args[1]
@@ -29,6 +37,27 @@ func main() {
 			return
 		}
 		fmt.Println(string(jsonOutput))
+
+	case "info":
+
+		torrentFileInfo := extractTorrentFileInformation(2)
+
+		// calculate SHA-1 hash of the bencoded info dictionary
+		infoHash := sha1.Sum(torrentFileInfo.encodedMap.Bytes())
+
+		pieceLength := torrentFileInfo.infoMap["piece length"].(int64)
+		pieces := torrentFileInfo.infoMap["pieces"].(string)
+
+		fmt.Printf("Tracker URL: %v\n", torrentFileInfo.bencodeMap["announce"].(string))
+		fmt.Printf("Length: %v\n", torrentFileInfo.infoMap["length"].(int64))
+		fmt.Printf("Info Hash: %x\n", infoHash)
+		fmt.Printf("Piece Length: %v\n", pieceLength)
+		fmt.Printf("Piece Hashes:\n")
+		var hash string
+		for i := 0; i < len(pieces); i += 20 {
+			hash = fmt.Sprintf("%x", pieces[i:i+20])
+			fmt.Printf("%v\n", hash)
+		}
 	}
 }
 
@@ -40,4 +69,53 @@ func decodeBencode(bencodedString string) (interface{}, error) {
 	}
 
 	return res, nil
+}
+
+func extractTorrentFileInformation(index int) *TorrentFile {
+	fileName := os.Args[index]
+	// fmt.Println("Torrent file: ", fileName)
+	// parse the input file
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("Error opening file: ", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// decode the bencoded data
+	bencodeData, err := bencode.Decode(file)
+	if err != nil {
+		fmt.Println("Error decoding bencode data: ", err)
+		os.Exit(1)
+	}
+
+	// convert bencoded data a map
+	bencodeMap, ok := bencodeData.(map[string]interface{})
+	if !ok {
+		fmt.Println("Error converting bencode data to map")
+		os.Exit(1)
+	}
+
+	// get the info map
+	infoMap, ok := bencodeMap["info"].(map[string]interface{})
+	if !ok {
+		fmt.Println("Error converting info data to map")
+		os.Exit(1)
+	}
+
+	// peerAddress := os.Args[3]
+	// fmt.Println("Peer address: ", peerAddress)
+
+	var encodedInfoMap bytes.Buffer
+	err = bencode.Marshal(&encodedInfoMap, infoMap)
+	if err != nil {
+		fmt.Println("Error marshalling info data: ", err)
+		os.Exit(1)
+	}
+
+	return &TorrentFile{
+		bencodeMap: bencodeMap,
+		infoMap:    infoMap,
+		encodedMap: encodedInfoMap,
+	}
 }
